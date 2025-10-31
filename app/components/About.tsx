7797e5d70d768140055866c761a325e71714d0de
juan <aswinjuanajk@gmail.com>
2025-10-31 17:47:24 +0700

    about page

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -3,27 +3,36 @@ import ImageCard from "./ui/ImageCard";
 const About = () => {
   return (
     <>
-      <div className="max-w-6xl mx-auto px-6">
-        <div className="flex flex-col md:justify-between">
-          <div className="flex flex-col gap-3 max-w-md">
-            <h2 className="font-bold text-5xl">About Eventnity</h2>
-            <p className="text-lg ">
-              Evenity is event management expertise that helps and facilitates
-              Event Planners in achieving the goal of carrying out an event.
+      <section className="mx-auto max-w-6xl py-4 px-5">
+        <div className="grid md:grid-cols-2">
+          <div className="">
+            <h1 className="text-4xl font-bold">About Eventnity</h1>
+            <p className="text-xl">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo saepe
+              consequuntur possimus voluptates dolorum velit veniam neque
+              reprehenderit vitae amet.
             </p>
-            <button className="bg-fuchsia-400 px-3 py-3 self-start rounded-2xl">
+            <button className="bg-amber-400 py-3 px-4 rounded-2xl">
               More About Us
             </button>
           </div>
 
-          <div>
-            <ImageCard
-              image="https://images.unsplash.com/photo-1459749411175-04bf5292ceea?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29uY2VydHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&q=60&w=600"
-              orientation="vertical"
-            />
+          <div className="relative flex justify-center">
+            <div className="absolute">
+              <ImageCard
+                image="/images/background-card1.jfif"
+                orientation="vertical"
+              ></ImageCard>
+            </div>
+            <div className="absolute bottom-0 right-0 translate-x-10 translate-y-10">
+              <ImageCard
+                image="/images/background-card1.jfif"
+                orientation="vertical"
+              ></ImageCard>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 };
