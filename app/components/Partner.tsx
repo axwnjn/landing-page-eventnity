05231e5d5787118d83b83de29ffe57477b690dc3
juan <aswinjuanajk@gmail.com>
2025-11-13 10:53:56 +0700

    client partner section

--- app/components/Partner.tsx
+++ app/components/Partner.tsx
@@ -1,8 +1,19 @@
+import Logo from "./ui/Logo";
+
 const Partner = () => {
   return (
-    <section className="flex flex-col items-center">
-      <h3 className="text-5xl font-semibold">Client Partner</h3>
-      <div className="grid grid-cols-4"></div>
+    <section className="flex flex-col items-center mb-50">
+      <h3 className="text-5xl font-semibold mb-12">Client Partner</h3>
+      <div className="grid grid-cols-4 gap-x-23 gap-y-14">
+        <Logo />
+        <Logo />
+        <Logo />
+        <Logo />
+        <Logo />
+        <Logo />
+        <Logo />
+        <Logo />
+      </div>
     </section>
   );
 };
--- app/components/ui/Logo.tsx
+++ app/components/ui/Logo.tsx
@@ -1,6 +1,17 @@
+import Image from "next/image";
+
 const Logo = () => {
   return (
-    <div className="w-[230px] h-[230px] flex items-center justify-center"></div>
+    <>
+      <div className="w-[230px] h-[143] flex items-center relative justify-center  rounded-2xl shadow-xl">
+        <Image
+          src="/images/Hulu_logo_(2018).svg.png"
+          alt="/logo"
+          fill
+          className="object-scale-down p-6"
+        />
+      </div>
+    </>
   );
 };
 
